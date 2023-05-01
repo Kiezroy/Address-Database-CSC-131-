df2bd8c856f76d66dd94fba95c9f26e992aba797
Kiezroy <kyletnguyen16@gmail.com>
2023-05-01 14:37:44 -0700

    Modified pom.xml to allow JUnit testing Added LoginFrameTest.java for JUnit testing

---
+++ Desktop/Database/src/test/java/happy/database/LoginFrameTest.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package happy.database;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Nick
+ */
+public class LoginFrameTest {
+    
+    public LoginFrameTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class LoginFrame.
+     */ 
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        
+        // Open the login frame
+        LoginFrame loginFrame = new LoginFrame();
+        loginFrame.setVisible(true);
+
+        // Check if the login frame is displayed correctly
+        assertTrue(loginFrame.isDisplayable());
+
+    }
+
+    
+}
